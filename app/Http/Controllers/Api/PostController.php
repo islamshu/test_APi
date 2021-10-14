ac3a3bde13a1d0291fb361687649585827330abf
islamshu12 <islamshu12@gmail.com>
2021-10-14 13:52:42 +0300

    edit post

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -9,10 +9,19 @@ use Illuminate\Http\Request;
 use App\Subscribe;
 use Illuminate\Support\Facades\Mail;
 use App\Http\Controllers\Api\BaseController;
-
+use Validator;
 class PostController extends BaseController
 {
     public function create(Request $request){
+        $validator = Validator::make($request->all(), [
+            'title' => 'required',
+            'body' => 'required',
+            'url' => 'required|url',
+        ]);
+    
+        if ($validator->fails()) {
+            return $this->sendError($validator->messages());
+        }
         $post = new Post();
         $post ->title = $request->title;
         $post->body = $request->body;
